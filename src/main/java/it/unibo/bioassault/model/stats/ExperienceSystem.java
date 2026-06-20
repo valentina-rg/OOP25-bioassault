1959e171d545f300228c66576774639449c851c0
Rebecca Scarcelli <rebeccascarcelli5@gmail.com>
2026-06-20 16:34:52 +0200

    Developing ExperienceSystem.java

--- src/main/java/it/unibo/bioassault/model/stats/ExperienceSystem.java
+++ src/main/java/it/unibo/bioassault/model/stats/ExperienceSystem.java
@@ -5,8 +5,23 @@ import java.util.List;
 
 public class ExperienceSystem {
     
-    private static final int MAX_LEVEL = 99;
+    private static final int MAX_LEVEL = 100;
     private static final int UPGRADE_CHOICES = 3;
-    private static final 
+    private static final double XP_BASE = 100.0;
+    private static final int STARTING_LEVEL = 1;
+
+    private int level = STARTING_LEVEL;
+    private double currentXP = 0;
+    private double xpToNextLevel;
+
+    private final PlayerStats stats;
+    private final list<Upgrade> upgradePool = new ArrayList<>();
+    private final list<Upgrade> acquiredUpgrade = new ArrayList<>();
+    private final list<LevelUpListener> listeners = new ArrayList<>();
+
+    public ExperienceSystem() {
+        
+    }
+
 
 }
