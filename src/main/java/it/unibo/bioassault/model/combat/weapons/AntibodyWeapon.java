140cb3df212d6be27577272c82c562d20c91e9b0
Francesco Prandini <francesco.prandini24@gmail.com>
2026-07-06 17:36:38 +0200

    sistemata direzione sparo

---
+++ src/main/java/it/unibo/bioassault/model/combat/weapons/AntibodyWeapon.java
@@ -0,0 +1,8 @@
+package it.unibo.bioassault.model.combat.weapons;
+
+public class AntibodyWeapon extends Weapon {
+
+    public AntibodyWeapon() {
+        super(10, 10.0f); // 10 danni, proiettile veloce
+    }
+}
