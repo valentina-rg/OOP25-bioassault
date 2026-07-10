a11629ec65ba87135f02f1dd1ec7f1821bb5a78b
Valentina Rigato <valentina.rigato@studio.unibo.it>
2026-07-10 12:48:52 +0200

    tolti i todo

--- src/test/java/it/unibo/bioassault/model/viruses/VirusTest.java
+++ src/test/java/it/unibo/bioassault/model/viruses/VirusTest.java
@@ -9,13 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class VirusTest {
 
     private static class TestVirus extends Virus {
-        // TODO vedi se usare il virus vero invece di testvirus
-        // TODO for loop in cui spawnin 10 mostri, poi test se ci sono effettivamente 10 mostri
-        // TODO fai 10 mostri, ne uccidi 5, ci sono 5 mostri?
-        // DONE spawn boss, c'è il boss?
-        // TODO danni ecc ecc al boss
-        // TODO spawn spawn boss e mostri, ci sono tutti?
-        // TODO test spostamento boss e mostri
+
         TestVirus(final int hp) {
             super(0, 0, ID.Enemy, new Handler(), hp, 1.0f);
         }
